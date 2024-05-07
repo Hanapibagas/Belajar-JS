dc5f708ed4e8e8e5b1369b07ecf1d40b504987a2
Hanapibagas <hanafibagas4@gmail.com>
2024-05-07 11:54:48 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let nama = 'Hanapi bagas pamungkas'
+let usia = 24
+let tinggiBadan = 174
+let beratBadan
+let pacar = null
+
+beratBadan = 60
+
+if (pacar == null) {
+   pacar = 'Belum mempunyai pacar'
+} else {
+   pacar = 'udah mempunyai pacar'
+}
+
+alert(`nama saya ${nama} usia saya ${usia} 
+tinggi badan saya ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
